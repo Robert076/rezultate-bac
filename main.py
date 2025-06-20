be7afa9b0058d0a489043974c2d6bf47ba59b59b
Robert076 <robertberes06@gmail.com>
2025-06-20 09:38:08 +0300

    logic fix

--- main.py
+++ main.py
@@ -6,12 +6,15 @@ class BColors:
     INCORRECT = '\033[91m'  
     NORMAL = '\033[0m'     
 
-while True:
-    x = requests.get('https://bacalaureat.edu.ro/2025/rapoarte/rezultate/frame.html')
-    
-    if x.status_code == 404:
-        print(f"{BColors.INCORRECT}Status: 404 Not Found{BColors.NORMAL}")
-    else:
-        print(f"{BColors.CORRECT}Status: {x.status_code} OK{BColors.NORMAL}")
-    
-    time.sleep(10) 
+try:
+    while True:
+        x = requests.get('https://bacalaureat.edu.ro/2025/rapoarte/rezultate/frame.html')
+        
+        if x.status_code != '200':
+            print(f"{BColors.INCORRECT}Status: {x.status_code} Not Found{BColors.NORMAL}")
+        else:
+            print(f"{BColors.CORRECT}Status: {x.status_code} OK{BColors.NORMAL}")
+        
+        time.sleep(10) 
+except KeyboardInterrupt:
+    print("Bye")
